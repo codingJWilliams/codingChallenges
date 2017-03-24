1ff92c7fcba6a0275a580c25f89039e4005fc4eb
Jay Williams <codingJWilliams@users.noreply.github.com>
2017-03-24 12:34:29 +0000

    Create 009.py

---
+++ 009/009.py
@@ -0,0 +1,1 @@
+import questions, randomfinalText = """Quiz v1.0!By: Jay Williams."""for i in range(1, 6):    currentQuestion = random.choice(random.choice(questions.subjects))    finalText += "\nQ{}: {}".format(i, currentQuestion[0])print(finalText)
