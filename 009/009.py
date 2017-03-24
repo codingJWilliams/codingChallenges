1d1f7131938f123927a28dde24b5ced7e2241233
Jay Williams <course@jaywilliams.me>
2017-03-24 17:13:08 +0000

    Fixed errors

--- 009/009.py
+++ 009/009.py
@@ -1,1 +1,11 @@
-import questions, randomfinalText = """Quiz v1.0!By: Jay Williams."""for i in range(1, 6):    currentQuestion = random.choice(random.choice(questions.subjects))    finalText += "\nQ{}: {}".format(i, currentQuestion[0])print(finalText)
+import questions, random
+finalText = """
+Quiz v1.0!
+By: Jay Williams.
+
+"""
+for i in range(1, 6):
+    currentQuestion = random.choice(random.choice(questions.subjects))
+    finalText += "\nQ{}: {}".format(i, currentQuestion[0])
+
+print(finalText)
